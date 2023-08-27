6e7b42d32fb1d61e280bdcfdf05c032122aaba07
Thomas Solecki <thomasjsolecki@gmail.com>
2023-08-27 10:52:59 -0700

    Iterator introduction

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+let list = [1, 2, 3];
+list = list.map(entry => entry + 1);
+console.log(list);
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
+    let list: Vec<_> = vec![1, 2, 3]
+        .iter()
+        .map(|x| x+1 )
+        .collect();
+    println!("{:?}", list);
+
+    let list_to_collect = vec![4, 5, 6];
+    let mut list_iter = list_to_collect
+        .iter()
+        .map(|x| x + 1);
+
+    let mut new_vector = vec![];
+    while let Some(x) = list_iter.next() {
+        new_vector.push(x);
+    }
+    println!("{:?}", new_vector);
 }
