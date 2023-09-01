4d7b010f996584ba089d330f051df34aae14a6d5
Thomas Solecki <thomasjsolecki@gmail.com>
2023-08-31 18:55:32 -0700

    Borrow checker practice

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,17 @@ fn add_one(item: &mut Item) {
     item.count += 1;
 }
 
+fn print_all(items: &Vec<Item>) {
+    for item in items {
+        println!("{:?}", item);
+    }
+}
+
 fn main() {
-    let mut item = Item {count: 1};
-    println!("{:?}", item);
-    add_one(&mut item);
-    println!("{:?}", item);
+    let mut items = vec![Item {count: 1}];
+    let first = items.get_mut(0);
+    let second = items.get_mut(1);
+    println!("{:?}", second);
+
+    print_all(&items);
 }
